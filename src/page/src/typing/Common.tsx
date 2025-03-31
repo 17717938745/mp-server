1186394fe91f6219d43bf5cfee5935d1703c4524
panchaohui <panchaohui@fofund.com.cn>
2025-03-31 16:44:57 +0800

    commit

--- src/page/src/typing/Common.tsx
+++ src/page/src/typing/Common.tsx
@@ -1,6 +1,6 @@
 export const PAGE_SIZE_LIST = [20, 50, 100, 200, 300, 500, 700, 1000]
 export const DEFAULT_PAGE = 1
-export const DEFAULT_LIMIT = 15
+export const DEFAULT_LIMIT = 20
 export const MAX_LIMIT = PAGE_SIZE_LIST.length > 0 ? PAGE_SIZE_LIST[PAGE_SIZE_LIST.length - 1] : 999999
 
 interface Env {
